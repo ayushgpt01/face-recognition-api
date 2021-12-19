0ecb4a3862b8400f94076320bd3fd6fc5424af5a
ayushgpt01 <ayushgpt043@gmail.com>
2021-12-19 16:59:06 +0530

    Security Changes

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,16 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '1fabd297ff474907933ebdd8d4498bcc'
+   });
+const handleAPIcall = (req,res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(res.status(400).json("unable to work with api"))
+}
+
 const handleImage = (req,res,db) => {
     const {id} = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +23,6 @@ const handleImage = (req,res,db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage,
+    handleAPIcall
 }
--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.post('/register',(req,res) => {register.handleRegister(req,res,db,bcrypt,sal
 app.get('/profile/:id',(req,res) => {profile.handleProfile(req,res,db)})
 
 app.put('/image',(req,res) => {image.handleImage(req,res,db)})
+app.post('/imageurl',(req,res) => {image.handleAPIcall(req,res)})
 
 app.listen(3000,()=> {
     console.log("App is running..");
