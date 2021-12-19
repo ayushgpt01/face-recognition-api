b9881e10659c38a3a3a88bd2636fe67a5d25a9ae
ayushgpt01 <ayushgpt043@gmail.com>
2021-12-19 17:37:39 +0530

    port

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.get('/profile/:id',(req,res) => {profile.handleProfile(req,res,db)})
 app.put('/image',(req,res) => {image.handleImage(req,res,db)})
 app.post('/imageurl',(req,res) => {image.handleAPIcall(req,res)})
 
-app.listen(3000,()=> {
-    console.log("App is running..");
+app.listen(process.env.PORT || 3000,()=> {
+    console.log(`App is running on ${process.env.PORT}`);
 });
